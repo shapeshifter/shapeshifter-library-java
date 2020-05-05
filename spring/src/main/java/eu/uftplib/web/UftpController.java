551833154ff3cd1b3fea11823ae328944207e311
Andre Broers <broersa@gmail.com>
2020-05-05 16:18:23 +0200

    added xsd validation

--- spring/src/main/java/eu/uftplib/web/UftpController.java
+++ spring/src/main/java/eu/uftplib/web/UftpController.java
@@ -1,6 +1,19 @@
 package eu.uftplib.web;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+
+import javax.xml.validation.Validator;
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +26,40 @@ public class UftpController {
 	public String uftp(HttpEntity<String> httpEntity) {
 		String xml = httpEntity.getBody();
 		System.out.println(xml);
+		if (!validateXML(xml)) return "ERROR";
 		return "OK";
+	}
+
+	private boolean validateXML(String xml) {
+		String xsdFilename = "";
+		switch ("AGR") {
+			case "AGR":
+				xsdFilename = "/UFTP-V1.01-agr.xsd"; 
+				break;
+			case "CRO":
+				xsdFilename = "/UFTP-V1.01-cro.xsd"; 
+				break;
+			case "DSO":
+				xsdFilename = "/UFTP-V1.01-dso.xsd"; 
+				break;
+		
+			default:
+				break;
+		}
+		File schemaFile = new File(xsdFilename);
+		Source xmlFile = new StreamSource(new File("web.xml"));
+		SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		StringReader reader = new StringReader(xml);
+		try {
+  			Schema schema = schemaFactory.newSchema(schemaFile);
+  			Validator validator = schema.newValidator();
+  			validator.validate(new StreamSource(reader));
+			System.out.println(xmlFile.getSystemId() + " is valid");
+			return true;
+		} catch (SAXException e) {
+  			System.out.println(xmlFile.getSystemId() + " is NOT valid reason:" + e);
+		} catch (IOException e) {}
+		return false;
 	}
 
 }
