b736cde81db8573adb7ea339318b659041df93fd
Andre Broers <broersa@gmail.com>
2020-05-05 14:28:18 +0200

    refactoring

--- spring/src/main/java/eu/uftplib/web/UftpController.java
+++ spring/src/main/java/eu/uftplib/web/UftpController.java
@@ -1,4 +1,4 @@
-package eu.uftplib;
+package eu.uftplib.web;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpEntity;
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
-public class MessagesController {
+public class UftpController {
 
 	@RequestMapping(value = "/api/messages", method = RequestMethod.POST, consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public String messages(HttpEntity<String> httpEntity) {
+	public String uftp(HttpEntity<String> httpEntity) {
 		String xml = httpEntity.getBody();
 		System.out.println(xml);
 		return "OK";
