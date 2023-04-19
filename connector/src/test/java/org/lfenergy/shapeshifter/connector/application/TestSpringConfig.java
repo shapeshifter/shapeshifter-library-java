7bf6833d8ca6033507496848c10c6be586b02855
Tom Wetjens <tomwetjens@gmail.com>
2023-04-19 10:04:11 +0200

    Add failsafe plugin. Upgrade deps (#27)
    * Remove some obsolete classes accidentally left behind. Upgrade deps. Add failsafe plugin so integration tests are run as part of the Maven build
    
    Signed-off-by: Tom Wetjens <tomwetjens@gmail.com>
    
    * Fix version property
    
    Signed-off-by: Tom Wetjens <tomwetjens@gmail.com>
    
    ---------
    
    Signed-off-by: Tom Wetjens <tomwetjens@gmail.com>

--- connector/src/test/java/org/lfenergy/shapeshifter/connector/application/TestSpringConfig.java
+++ connector/src/test/java/org/lfenergy/shapeshifter/connector/application/TestSpringConfig.java
@@ -7,16 +7,13 @@ package org.lfenergy.shapeshifter.connector.application;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
 @Configuration
 @EnableSpringConfigured
-@Import({UftpControllerTestApp.class})
-@ComponentScan(basePackages = {"org.lfenergy.shapeshifter"}, excludeFilters = {
-    @ComponentScan.Filter(type = FilterType.CUSTOM, value = ExcludeTestMappingFilter.class)
-})
+@Import(UftpControllerTestApp.class)
+@ComponentScan(basePackages = "org.lfenergy.shapeshifter")
 @EnableConfigurationProperties
-public class TestSpringConfigExcludingTestMappings {
+public class TestSpringConfig {
 }
