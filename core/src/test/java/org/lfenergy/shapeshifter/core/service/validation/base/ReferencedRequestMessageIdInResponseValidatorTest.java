42601165aace3c1809187198a2d577d9fab1bcbe
Marten Meijboom <marten1998@hotmail.com>
2024-01-29 14:14:00 +0100

    Provide message type based on response type to UftpMessageSupport in ReferencedRequestMessageIdInResponseValidator (#73) (#74)
    Signed-off-by: Marten Meijboom <marten.meijboom@edsn.nl>
    Co-authored-by: Marten Meijboom <marten.meijboom@edsn.nl>

--- core/src/test/java/org/lfenergy/shapeshifter/core/service/validation/base/ReferencedRequestMessageIdInResponseValidatorTest.java
+++ core/src/test/java/org/lfenergy/shapeshifter/core/service/validation/base/ReferencedRequestMessageIdInResponseValidatorTest.java
@@ -2,6 +2,8 @@ package org.lfenergy.shapeshifter.core.service.validation.base;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.lfenergy.shapeshifter.api.FlexOffer;
+import org.lfenergy.shapeshifter.api.FlexOfferResponse;
 import org.lfenergy.shapeshifter.api.FlexRequest;
 import org.lfenergy.shapeshifter.api.FlexRequestResponse;
 import org.lfenergy.shapeshifter.api.PayloadMessageResponseType;
@@ -23,6 +25,7 @@ import static org.mockito.BDDMockito.given;
 @ExtendWith(MockitoExtension.class)
 class ReferencedRequestMessageIdInResponseValidatorTest {
     private static final String FLEX_REQUEST_MESSAGE_ID = UUID.randomUUID().toString();
+    private static final String FLEX_OFFER_MESSAGE_ID = UUID.randomUUID().toString();
     private static final String CONVERSATION_ID = UUID.randomUUID().toString();
     @Mock
     private UftpMessageSupport messageSupport;
@@ -32,7 +35,9 @@ class ReferencedRequestMessageIdInResponseValidatorTest {
 
     private final UftpParticipant sender = new UftpParticipant("example.com", USEFRoleType.DSO);
     private final PayloadMessageResponseType flexRequestResponse = new FlexRequestResponse();
+    private final PayloadMessageResponseType flexOfferResponse = new FlexOfferResponse();
     private final FlexRequest flexRequest = new FlexRequest();
+    private final FlexOffer flexOffer = new FlexOffer();
 
     @Test
     void appliesTo() {
@@ -64,13 +69,37 @@ class ReferencedRequestMessageIdInResponseValidatorTest {
     }
 
     @Test
-    void invalid_whenReferenceInResponseIsNotKnown() {
+    void valid_whenFlexOfferReferenceInResponseIsKnown() {
+        var uftpMessage = UftpMessageFixture.createIncomingResponse(sender, flexOfferResponse);
+
+        ((FlexOfferResponse) flexOfferResponse).setFlexOfferMessageID(FLEX_OFFER_MESSAGE_ID);
+        flexOfferResponse.setConversationID(CONVERSATION_ID);
+        given(messageSupport.findReferencedMessage(uftpMessage.referenceToPreviousMessage(FLEX_OFFER_MESSAGE_ID, CONVERSATION_ID,
+                FlexOffer.class))).willReturn(Optional.of(flexOffer));
+
+        assertThat(testSubject.isValid(uftpMessage)).isTrue();
+    }
+
+    @Test
+    void invalid_whenFlexRequestReferenceInResponseIsNotKnown() {
         var uftpMessage = UftpMessageFixture.createIncomingResponse(sender, flexRequestResponse);
 
         ((FlexRequestResponse) flexRequestResponse).setFlexRequestMessageID(FLEX_REQUEST_MESSAGE_ID);
         flexRequestResponse.setConversationID(CONVERSATION_ID);
         given(messageSupport.findReferencedMessage(uftpMessage.referenceToPreviousMessage(FLEX_REQUEST_MESSAGE_ID, CONVERSATION_ID,
                 FlexRequest.class))).willReturn(Optional.empty());
+
+        assertThat(testSubject.isValid(uftpMessage)).isFalse();
+    }
+
+    @Test
+    void valid_whenFlexOfferReferenceInResponseIsNotKnown() {
+        var uftpMessage = UftpMessageFixture.createIncomingResponse(sender, flexOfferResponse);
+
+        ((FlexOfferResponse) flexOfferResponse).setFlexOfferMessageID(FLEX_OFFER_MESSAGE_ID);
+        flexOfferResponse.setConversationID(CONVERSATION_ID);
+        given(messageSupport.findReferencedMessage(uftpMessage.referenceToPreviousMessage(FLEX_OFFER_MESSAGE_ID, CONVERSATION_ID,
+                FlexOffer.class))).willReturn(Optional.empty());
 
         assertThat(testSubject.isValid(uftpMessage)).isFalse();
     }
